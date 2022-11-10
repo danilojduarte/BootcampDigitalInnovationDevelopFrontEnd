c70c772fce8d6024aff36b377f5c185dc60ad8c1
DaniloDuarte <danilojoseduarte@gmail.com>
2022-11-10 14:45:47 -0300

    Aula js finalizada

--- Aulas/03-Primeiros Passos JS/01aulas/aula.js
+++ Aulas/03-Primeiros Passos JS/01aulas/aula.js
@@ -1,30 +1,44 @@
-// Formas de Declarar Variaveis
-
-
-// Desafio Distancia por km consumidos
-const valorCombustivel  = 5.49;
-const kmPorLitros = 9;
-const distanciaEmKm = 100;
+// DESAFIO
+const valorEtanol = 5.79;
+const valorGasolina = 6.66;
+const consumokm = 10;
+const tipoConbustivel = "gasolina";
 
 const distanciaPercorrida = distanciaEmKm / kmPorLitros;
 
-const valorGasto = distanciaPercorrida * valorCombustivel;  
+if (valorEtanol === "Etanol") {
+  const valorGasto = distanciaPercorrida * valorEtanol;
+  console.log(consumokm.toFixed(2);
+} else {
+  const valorGasto = distanciaPercorrida * valorGasolina;
+  console.log(consumokm.toFixed(2));
+}
+// Operadores Codicionais
+// const numero = 2;
+// const numeroPar = (numero % 2) === 0;
 
-console.log(valorGasto.toFixed(2));
+// if (numeroPar) {
+//     console.log('Par');
+// } else {
+//     console.log('Impar');
+// }
+
+// Formas de Declarar Variaveis
+
+// Desafio Distancia por km consumidos
+// const valorCombustivel  = 5.49;
+// const kmPorLitros = 9;
+// const distanciaEmKm = 100;
+
+// const distanciaPercorrida = distanciaEmKm / kmPorLitros;
+
+// const valorGasto = distanciaPercorrida * valorCombustivel;
+
+// console.log(valorGasto.toFixed(2));
 // Finalizado
-
-
-
-
-
-
-
-
-
 
 //     let variavel = 10;
 
 //     // const pi = 3.14;
-
 
 // console.log(variavel);
