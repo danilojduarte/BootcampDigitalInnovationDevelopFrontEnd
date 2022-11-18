d610a132b6b6e8f46c7f62d72b3740eeba501ae0
DaniloDuarte <danilojoseduarte@gmail.com>
2022-11-18 14:40:32 -0300

    Aula Finalizada

---
+++ Aulas/03-Primeiros Passos JS/01aulas/04 Arrays/arrays.js
@@ -0,0 +1,19 @@
+
+const numeros =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    console.log(numero);
+    if (numero  % 2 === 0 ) {
+        console.log(numero); 
+    // } else {
+    //     console.log(numero + " é impar ");
+    // }
+}
+
+
+
+// const numero = 5;
+// for (let i = 1; i < 10; i++) {
+//     console.log(i * numero);
+// }
