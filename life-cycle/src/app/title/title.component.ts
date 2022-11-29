8b43bf064605a3371e298dd1be51f9f34d71be36
DaniloDuarte <danilojoseduarte@gmail.com>
2022-11-29 00:58:25 -0300

    Aula Angular finalizada

--- life-cycle/src/app/title/title.component.ts
+++ life-cycle/src/app/title/title.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class TitleComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+
 
 }
