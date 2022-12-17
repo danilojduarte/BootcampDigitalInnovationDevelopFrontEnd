f06eeedf3a3e0d256f553c663815ba5c3c96cf4c
DaniloDuarte <danilojoseduarte@gmail.com>
2022-12-17 01:57:02 -0300

    Atribuindo processos dinamicos

--- Praticas/Desafio07 -Clone BuzzFeed/projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ Praticas/Desafio07 -Clone BuzzFeed/projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quizz',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizz.component.css']
 })
 export class QuizzComponent implements OnInit {
+    title:string = ""
+
+    questions:any
+    questionSelected:any
+
+    answers:string[] = []
+    answersSelected:string = ""
+
+    questionIndex:number = 0
+    questionMaxIndex:number = 0
+
+    finished:boolean = false
+
 
   constructor() { }
 
   ngOnInit(): void {
+    if(quizz_questions)
+    this.finished = false
   }
 
 }
