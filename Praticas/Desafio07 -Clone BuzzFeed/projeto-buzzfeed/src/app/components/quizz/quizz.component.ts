a0e6514c5d0e70d1dc78f823ca4b6a756c2a24b6
DaniloDuarte <danilojoseduarte@gmail.com>
2022-12-19 01:03:20 -0300

    Projeto Finalizado

--- Praticas/Desafio07 -Clone BuzzFeed/projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ Praticas/Desafio07 -Clone BuzzFeed/projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -39,6 +39,7 @@ export class QuizzComponent implements OnInit {
 
   playerChoose(value:string){
     this.answers.push(value)
+    this.nextStep()
   }
 
   async nextStep(){
@@ -47,8 +48,24 @@ export class QuizzComponent implements OnInit {
     if(this.questionMaxIndex > this.questionIndex){
       this.questionSelected = this.questions[this.questionIndex]
     } else {
+      const finalAnswer:string = await this.checkResult(this.answers)
       this.finished = true
+      this.answersSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
     }
   }
 
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, i, arr)=>{
+      if (
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ){
+        return previous
+      } else {
+        return current
+      }
+    
+    })
+    return result
+  }
 }
